882346b2d5ca1d42bb57bf0e1b237dc326ecf6ca
Anshuman Pati <anshupati2012@gmail.com>
2024-10-21 17:51:40 +0530

    Setup Github Login

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import { useAuthActions } from "@convex-dev/auth/react";
+
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
@@ -13,8 +15,14 @@ interface SignInCardProps {
 };
 
 export const SignInCard = ({ setState }: SignInCardProps) => {
+    const { signIn } = useAuthActions();
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    const  handleProviderSignIn = (value: "github" | "google") => {
+        signIn(value);
+    }
     return (
         <Card className="w-full h-full p-8">
             <CardHeader className="px-0 pt-0">
@@ -60,7 +68,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
                 </Button>
                 <Button 
                 disabled={false}
-                onClick={() => {}}
+                onClick={() => handleProviderSignIn("github")}
                 variant={"outline"}
                 className="w-full relative"
                 >
