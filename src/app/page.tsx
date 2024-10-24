dec0a6822b0da272cf81bb4e1c08d1e2c8d3a925
Anshuman Pati <anshupati2012@gmail.com>
2024-10-24 23:36:41 +0530

    github signout fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,21 @@
 
 import { Button } from "@/components/ui/button";
 import { useAuthActions } from "@convex-dev/auth/react";
+import { useRouter } from 'next/router';
 
 export default function Home() {
   const { signOut } = useAuthActions();
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    await signOut(); // Sign out the user
+    router.push('/auth'); // Redirect to /auth
+  };
+
   return (
     <div>
       Logged In!
-      <Button onClick={ () => signOut()}>signout</Button>
+      <Button onClick={handleSignOut}>Sign Out</Button>
     </div>
   );
 }
