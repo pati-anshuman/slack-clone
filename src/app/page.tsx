7f40d6daef1560d1c936b15017a16884c4842eee
Anshuman Pati <anshupati2012@gmail.com>
2024-10-28 17:08:14 +0530

    google sign-in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { useAuthActions } from "@convex-dev/auth/react";
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
   const { signOut } = useAuthActions();
--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -20,7 +20,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const  handleProviderSignIn = (value: "github" | "google") => {
+    const  onProviderSignIn = (value: "github" | "google") => {
         signIn(value);
     }
     return (
@@ -59,7 +59,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
                 <div className="flex flex-col gap-y-2.5">
                 <Button 
                 disabled={false}
-                onClick={() => {}}
+                onClick={() => onProviderSignIn("google")}
                 variant={"outline"}
                 className="w-full relative"
                 >
@@ -68,7 +68,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
                 </Button>
                 <Button 
                 disabled={false}
-                onClick={() => handleProviderSignIn("github")}
+                onClick={() => onProviderSignIn("github")}
                 variant={"outline"}
                 className="w-full relative"
                 >
